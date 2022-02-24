c686eb7ecda2a9588683b10981716049721987bd
Robert <comps@diprobe.com>
2022-02-24 19:42:10 +0100

    Skin Large Display Moto

--- app/src/main/java/info/nightscout/androidaps/skins/SkinLargeDisplay.kt
+++ app/src/main/java/info/nightscout/androidaps/skins/SkinLargeDisplay.kt
@@ -12,8 +12,8 @@ import javax.inject.Singleton
 class SkinLargeDisplay @Inject constructor(private val config: Config): SkinInterface {
 
     override val description: Int get() = R.string.largedisplay_description
-    override val mainGraphHeight: Int get() = 400
-    override val secondaryGraphHeight: Int get() = 150
+    override val mainGraphHeight: Int get() = 385
+    override val secondaryGraphHeight: Int get() = 270
 
     override fun preProcessLandscapeOverviewLayout(dm: DisplayMetrics, view: View, isLandscape: Boolean, isTablet: Boolean, isSmallHeight: Boolean) {
         super.preProcessLandscapeOverviewLayout(dm, view, isLandscape, isTablet, isSmallHeight)
